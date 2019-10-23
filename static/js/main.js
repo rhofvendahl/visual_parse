cd826395dc11a4b296978602b009479b230adc2f
russell Hofvendahl <rhofvendahl@gmail.com>
2019-10-22 19:00:42 -0700

    Create UpdateManager class to manage updates

--- static/js/main.js
+++ static/js/main.js
@@ -4,79 +4,19 @@ $(document).ready(function() {
         resizeHeight: false
     });
 
-    parseTree = new ParseTree();
-
-    experiment = new Experiment();
-    experiment.fit();
-
     var input = $('#text');
     input.val('This is a dependency parse tree. Click, hover and type to explore!');
     input.focus();
     input.prop('selectionStart', input.val().length);
     input.prop('selectionEnd', input.val().length);
 
-    var dirty = false;
-    var updating = false;
+    updateManager = new UpdateManager(input);
 
-    function startUpdating() {
-        updating = true;
-        text = input.val();
-        dirty = false;
-        fetch('/parse', {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                text: text
-            })
-        }).then(function(response) {
-            return response.json();
-        }).then(function(json) {
-            // console.log(json);
-            parseTree.render(json.tokens);
-        }).then(function() {
-            if (dirty)  {
-                startUpdating();
-            } else {
-                finishUpdating();
-            }
-        });
-    }
-
-    function finishUpdating() {
-        updating = false;
-        text = input.val();
-        fetch('/parse_experimental_only', {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                text: text
-            })
-        }).then(function(response) {
-            return response.json();
-        }).then(function(json) {
-            // console.log(json);
-            experiment.render(json.model);
-        });
-    }
-
-    setTimeout(startUpdating, 1700, input.val());
+    setTimeout(updateManager.startUpdating, 1700, input.val());
     $('#normal').animate({
         width: '60%'
     }, 2000, function() {
-        experiment.fit();
-        parseTree.fit();
-    });
-
-    input.on('input', function(event) {
-        dirty = true;
-        if (!updating) {
-            startUpdating();
-        }
+        updateManager.experiment.fit();
+        updateManager.parseTree.fit();
     });
 });
--- static/js/update_manager.js
+++ static/js/update_manager.js
@@ -1,77 +1,63 @@
-var UpdateManager = function() {
+var UpdateManager = function(input) {
     var self = this;
 
     self.parseTree = new ParseTree();
     self.experiment = new Experiment();
 
-    var container = document.getElementById('parse-tree');
-    var data = {nodes: self.nodes, edges: self.edges};
-    var options = {};
-    self.network = new vis.Network(container, data, options);
+    self.input = input;
 
-    self.tokenNodes = []
+    self.dirty = false;
+    self.updating = false;
 
-    self.fit = function() {
-        var fitOptions = {
-            nodes: self.nodes.getIds()
-        };
-        self.network.fit(fitOptions);
-    };
-
-    //// DOESN'T BELONG
-    // RETURN TOKENNODE BY ID
-    self.getTokenNode = function(id) {
-        var match = null;
-        self.tokenNodes.forEach(function(tokenNode) {
-            if (tokenNode.id == id) match = tokenNode;
-        });
-        return match;
-    }
-
-    // GENERATE TOKENNODES FROM TOKENS, render
-    self.importSubtree = function(tokens, token) {
-        tokenNode = new TokenNode(self, token);
-        self.tokenNodes.push(tokenNode);
-        token.child_ids.forEach(function(childId) {
-            self.importSubtree(tokens, tokens[childId])
-        });
-    }
-
-    self.renderSubtree = function(tokenNode) {
-        tokenNode.render();
-        tokenNode.children.forEach(function(child) {
-            self.renderSubtree(child);
-        });
-    }
-
-    //// BELONS HERE
-    // PROCESS QUERY, DISPLAY RESULTS
-    self.render = function(tokens) {
-        // console.log('ParseTree: rendering...')
-        self.tokenNodes = []
-        tokens.forEach(function(token) {
-            if (token.dep == 'ROOT') {
-                self.importSubtree(tokens, token);
-                self.renderSubtree(self.getTokenNode(token.id));
+    self.startUpdating = function() {
+        self.updating = true;
+        self.dirty = false;
+        fetch('/parse', {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                text: input.val()
+            })
+        }).then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            // console.log(json);
+            self.parseTree.render(json.tokens);
+        }).then(function() {
+            if (self.dirty)  {
+                self.startUpdating();
+            } else {
+                self.finishUpdating();
             }
         });
+    }
 
-        // remove additional nodes
-        self.nodes.getIds().forEach(function(id) {
-            if (!self.getTokenNode(id)) {
-                self.nodes.remove(id);
-            }
+    self.finishUpdating = function() {
+        self.updating = false;
+        fetch('/parse_experimental_only', {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                text: input.val()
+            })
+        }).then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            // console.log(json);
+            self.experiment.render(json.model);
         });
-        // console.log('ParseTree: render complete.')
-    };
+    }
 
-    //// BELONGS HERE
-    // COLLAPSE SUBTREE WHEN CLICKED
-    self.network.on('click', function(properties) {
-        if (properties.nodes.length > 0) {
-            tokenNode = self.getTokenNode(properties.nodes[0]);
-            tokenNode.collapsed = !tokenNode.collapsed;
-            self.renderSubtree(tokenNode);
+    self.input.on('input', function(event) {
+        self.dirty = true;
+        if (!self.updating) {
+            self.startUpdating();
         }
     });
 }
