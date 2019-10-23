b06edb9ab714bd02c12ee9fd3394f7fac2f45ba7
russell Hofvendahl <rhofvendahl@gmail.com>
2019-10-22 18:45:27 -0700

    replace 'is typing' code with chained promises (way smoother, faster)

--- static/js/main.js
+++ static/js/main.js
@@ -11,13 +11,17 @@ $(document).ready(function() {
 
     var input = $('#text');
     input.val('This is a dependency parse tree. Click, hover and type to explore!');
-    console.log(input.val());
     input.focus();
     input.prop('selectionStart', input.val().length);
     input.prop('selectionEnd', input.val().length);
 
-    function updateParseTree(text) {
-        // console.log('Main: updating (ParseTree)...')
+    var dirty = false;
+    var updating = false;
+
+    function startUpdating() {
+        updating = true;
+        text = input.val();
+        dirty = false;
         fetch('/parse', {
             method: 'post',
             headers: {
@@ -33,12 +37,17 @@ $(document).ready(function() {
             // console.log(json);
             parseTree.render(json.tokens);
         }).then(function() {
-            // console.log('Main: update complete.')
+            if (dirty)  {
+                startUpdating();
+            } else {
+                finishUpdating();
+            }
         });
     }
 
-    function updateExperiment(text) {
-        // console.log('Main: updating (Experiment)...')
+    function finishUpdating() {
+        updating = false;
+        text = input.val();
         fetch('/parse_experimental_only', {
             method: 'post',
             headers: {
@@ -53,107 +62,21 @@ $(document).ready(function() {
         }).then(function(json) {
             // console.log(json);
             experiment.render(json.model);
-        }).then(function() {
-            // console.log('Main: update complete.')
         });
     }
 
-    function updateAll(text) {
-        // console.log('Main: updating (all)...')
-        fetch('/parse_experimental_also', {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                text: text
-            })
-        }).then(function(response) {
-            return response.json();
-        }).then(function(json) {
-            // console.log(json);
-            parseTree.render(json.tokens);
-            experiment.render(json.model);
-        }).then(function() {
-            // console.log('Main: update complete.')
-        });
-    }
-
-    setTimeout(updateAll, 1700, input.val());
+    setTimeout(startUpdating, 1700, input.val());
     $('#normal').animate({
         width: '60%'
     }, 2000, function() {
-        // update(input.value)
         experiment.fit();
         parseTree.fit();
-        // experiment.network.redraw();
-        // parseTree.network.redraw();
-        // TODO: check 'experimental' box
     });
 
-    // ensures updates aren't constant while user types
-    var activeTimeouts = 0;
     input.on('input', function(event) {
-        var updated = false;
-        if (activeTimeouts == 0) {
-            updateAll(input.val()); // executes at start of typing session
-            console.log('User starts typing.');
-            var updated = true;
+        dirty = true;
+        if (!updating) {
+            startUpdating();
         }
-        activeTimeouts += 1
-        setTimeout(function() {
-            activeTimeouts -= 1;
-            if (activeTimeouts == 0 && !updated) {
-                updateAll(input.val()); // executes to cap off typing session
-                console.log('User stops typing.');
-            }
-        }, 1000)
     });
-
-    setInterval(function() {
-        if (activeTimeouts > 1) {
-            updateParseTree(input.val()) // executes at intervals during typing session
-            console.log('User is typing.');
-        }
-    }, 200);
 });
-
-// window.onload = function() {
-//   visual = new Visual();
-//
-//   var page = document.getElementById('page');
-//   visual.update(page.value);
-//   console.log(page);
-//   page.focus();
-//   // page.selectionStart = page.value.length;
-//   // page.selectionEnd = page.value.length;
-//
-//   var dirty = false;
-//   var overload = 0;
-//   var maxUpdateFrequency = 1;
-//
-//   page.oninput = function(event) {
-//     dirty = true;
-//
-//     // update if not overloaded
-//     if (overload == 0) {
-//       visual.update(page.value)
-//       dirty = false;
-//
-//       // increment overload for a bit after updating
-//       overload += 1
-//       setTimeout(function() {
-//         overload -= 1;
-//
-//         // update after waiting in case dirty
-//         if (dirty) {
-//           visual.update(page.value);
-//           dirty = false;
-//         }
-//       }, 1000 / maxUpdateFrequency)
-//     }
-//   };
-// };
-//
-// // at some point make it so it sends a new request as soon as the old is resolved, or a bit after
