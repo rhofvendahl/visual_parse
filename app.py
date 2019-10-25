474385acd2d8f8628b1f8a678c75a56e4bfd8c3d
russell Hofvendahl <rhofvendahl@gmail.com>
2019-10-24 23:53:05 -0700

    cut out event2mind

--- app.py
+++ app.py
@@ -1,14 +1,65 @@
+#############################################################
+import os
+_proc_status = '/proc/%d/status' % os.getpid()
+
+_scale = {'kB': 1024.0, 'mB': 1024.0*1024.0,
+          'KB': 1024.0, 'MB': 1024.0*1024.0}
+
+def _VmB(VmKey):
+    '''Private.
+    '''
+    global _proc_status, _scale
+     # get pseudo file  /proc/<pid>/status
+    try:
+        t = open(_proc_status)
+        v = t.read()
+        t.close()
+    except:
+        return 0.0  # non-Linux?
+     # get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
+    i = v.index(VmKey)
+    v = v[i:].split(None, 3)  # whitespace
+    if len(v) < 3:
+        return 0.0  # invalid format?
+     # convert Vm value to bytes    global _proc_status, _scale
+
+    return float(v[1]) * _scale[v[2]]
+
+
+def memory(since=0.0):
+    '''Return memory usage in bytes.
+    '''
+    return _VmB('VmSize:') - since
+
+
+def resident(since=0.0):
+    '''Return resident memory usage in bytes.
+    '''
+    return _VmB('VmRSS:') - since
+
+
+def stacksize(since=0.0):
+    '''Return stack size in bytes.
+    '''
+    return _VmB('VmStk:') - since
+    ################################################################
+print('pre flask and stuff:', memory() / 1000000, "MB")
+
 from flask import Flask, render_template, request, jsonify
-from modl import Model
+print('post flask, pre modl:', memory() / 1000000, "MB")
+
+from modl import Model, nlp
+print('post modl:', memory() / 1000000, "MB")
 
 app = Flask(__name__)
 
-import spacy
-print('Loading en_core_web_sm...')
-nlp = spacy.load('en_core_web_sm')
-print('Load complete.')
+# import spacy
+# print('Loading en_core_web_sm...')
+# nlp = spacy.load('en_core_web_sm')
+# print('Load complete.')
 
 model = Model()
+print('pre flask and stuff:', memory() / 1000000, "MB")
 
 def get_token_list(doc_instance):
     noun_chunk_tokens = [chunk.root for chunk in doc_instance.noun_chunks]
@@ -85,6 +136,7 @@ def parse_experimental_only():
     return jsonify({
         'model': get_model_dict(model)
     })
+print('right about to run:', memory() / 1000000, "MB")
 
 if __name__ == "__main__":
     app.run()
