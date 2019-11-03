3e303e3946d3dde67ac21638de7957dc60de0087
russell Hofvendahl <rhofvendahl@gmail.com>
2019-11-03 13:15:27 -0800

    trim event2mind stuff out

--- app.py
+++ app.py
@@ -1,55 +1,55 @@
-#############################################################
-import os
-_proc_status = '/proc/%d/status' % os.getpid()
-
-_scale = {'kB': 1024.0, 'mB': 1024.0*1024.0,
-          'KB': 1024.0, 'MB': 1024.0*1024.0}
-
-def _VmB(VmKey):
-    '''Private.
-    '''
-    global _proc_status, _scale
-     # get pseudo file  /proc/<pid>/status
-    try:
-        t = open(_proc_status)
-        v = t.read()
-        t.close()
-    except:
-        return 0.0  # non-Linux?
-     # get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
-    i = v.index(VmKey)
-    v = v[i:].split(None, 3)  # whitespace
-    if len(v) < 3:
-        return 0.0  # invalid format?
-     # convert Vm value to bytes    global _proc_status, _scale
-
-    return float(v[1]) * _scale[v[2]]
-
-
-def memory(since=0.0):
-    '''Return memory usage in bytes.
-    '''
-    return _VmB('VmSize:') - since
-
-
-def resident(since=0.0):
-    '''Return resident memory usage in bytes.
-    '''
-    return _VmB('VmRSS:') - since
-
-
-def stacksize(since=0.0):
-    '''Return stack size in bytes.
-    '''
-    return _VmB('VmStk:') - since
-    ################################################################
-print('pre flask and stuff:', memory() / 1000000, "MB")
+# #############################################################
+# import os
+# _proc_status = '/proc/%d/status' % os.getpid()
+#
+# _scale = {'kB': 1024.0, 'mB': 1024.0*1024.0,
+#           'KB': 1024.0, 'MB': 1024.0*1024.0}
+#
+# def _VmB(VmKey):
+#     '''Private.
+#     '''
+#     global _proc_status, _scale
+#      # get pseudo file  /proc/<pid>/status
+#     try:
+#         t = open(_proc_status)
+#         v = t.read()
+#         t.close()
+#     except:
+#         return 0.0  # non-Linux?
+#      # get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
+#     i = v.index(VmKey)
+#     v = v[i:].split(None, 3)  # whitespace
+#     if len(v) < 3:
+#         return 0.0  # invalid format?
+#      # convert Vm value to bytes    global _proc_status, _scale
+#
+#     return float(v[1]) * _scale[v[2]]
+#
+#
+# def memory(since=0.0):
+#     '''Return memory usage in bytes.
+#     '''
+#     return _VmB('VmSize:') - since
+#
+#
+# def resident(since=0.0):
+#     '''Return resident memory usage in bytes.
+#     '''
+#     return _VmB('VmRSS:') - since
+#
+#
+# def stacksize(since=0.0):
+#     '''Return stack size in bytes.
+#     '''
+#     return _VmB('VmStk:') - since
+#     ################################################################
+# print('pre flask and stuff:', memory() / 1000000, "MB")
 
 from flask import Flask, render_template, request, jsonify
-print('post flask, pre modl:', memory() / 1000000, "MB")
+# print('post flask, pre modl:', memory() / 1000000, "MB")
 
 from modl import Model, nlp
-print('post modl:', memory() / 1000000, "MB")
+# print('post modl:', memory() / 1000000, "MB")
 
 app = Flask(__name__)
 
